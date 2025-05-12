44934943c9531cc7b67581714ccb73292b45ec3b
mhdfaruqi23 <muhammad.faruqi35@gmail.com>
2025-05-12 23:48:11 +0700

    feat: Connect to MongoDB

---
+++ server.js
@@ -0,0 +1,21 @@
+import { configDotenv } from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+configDotenv();
+
+const app = express();
+const PORT = 3003;
+
+mongoose.connect(process.env.MONGODB_URL);
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to MongoDB"));
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(PORT, () => console.log(`Server started in port: ${PORT}`));
