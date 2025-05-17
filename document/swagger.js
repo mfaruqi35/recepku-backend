444e36c11d0bb8384eb95bccd68a460360c921ff
mhdfaruqi23 <muhammad.faruqi35@gmail.com>
2025-05-17 19:55:17 +0700

    feat: add some documentation

---
+++ document/swagger.js
@@ -0,0 +1,28 @@
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Recepku API Documentation",
+      version: "1.0.0",
+      description: "Dokumentasi API untuk Website Recepku",
+    },
+    servers: [
+      {
+        url: "https://recepku-backend.vercel.app",
+      },
+    ],
+    security: [{ bearerAuth: [] }],
+  },
+  apis: ["./routes/*.js"], // Pastikan path sesuai
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+const swaggerDocs = (app) => {
+  app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerSpec)); // Gunakan "/docs"
+};
+
+export default swaggerDocs;
